fbcd8a5f9c749d2f655d09007533f46c09f705ec
AliAmmar2 <alialiamam.ss@gmail.com>
2026-06-17 16:11:55 +0300

    reset password added

--- frontend/src/app/_clients/student/student.client.ts
+++ frontend/src/app/_clients/student/student.client.ts
@@ -127,7 +127,7 @@ export class StudentClient {
 
  public forgotPassword(universityEmail: string): Observable<any> {
   return this.http.post(
-    `${environment.apiUrl}/auth/forgot-password`,
+    `${environment.apiUrl}/forgot-password`,
     {
       universityEmail
     }
@@ -136,7 +136,7 @@ export class StudentClient {
 
 public resetPassword(payload: { token: string; newPassword: string }) {
   return this.http.post(
-    `${environment.apiUrl}/auth/reset-password`,
+    `${environment.apiUrl}/reset-password`,
     payload
   );
 }
