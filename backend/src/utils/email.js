611fe48e66a06529e498bc0584eff37ef09bf768
AliAmmar2 <alialiamam.ss@gmail.com>
2026-07-07 10:37:08 +0300

    trying resend instead of nodemailer

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -1,81 +1,10 @@
-// const nodemailer = require("nodemailer");
-//
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS
-//   }
-// });
-//
-// exports.sendVerificationEmail = async (
-//   email,
-//   link
-// ) => {
-//   await transporter.sendMail({
-//     from: process.env.EMAIL_USER,
-//     to: email,
-//     subject: "Verify your university account",
-//     html: `
-//       <h2>Email Verification</h2>
-//       <p>Click below to verify your account:</p>
-//       <a href="${link}">
-//         Verify Email
-//       </a>
-//     `
-//   });
-// };
-//
-// exports.sendPasswordResetEmail = async (
-//   email,
-//   link
-// ) => {
-//   await transporter.sendMail({
-//     from: process.env.EMAIL_USER,
-//     to: email,
-//     subject: "Reset your password",
-//     html: `
-//       <h2>Password Reset</h2>
-//
-//       <p>
-//         A request was made to reset your password.
-//       </p>
-//
-//       <p>
-//         Click the link below to choose a new password:
-//       </p>
-//
-//       <a href="${link}">
-//         Reset Password
-//       </a>
-//
-//       <p>
-//         This link expires in 30 minutes.
-//       </p>
-//
-//       <p>
-//         If you did not request this reset,
-//         you can safely ignore this email.
-//       </p>
-//     `
-//   });
-// };
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 exports.sendVerificationEmail = async (email, link) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+  await resend.emails.send({
+    from: process.env.EMAIL_FROM,
     to: email,
     subject: "Verify your university account",
     html: `
@@ -87,8 +16,8 @@ exports.sendVerificationEmail = async (email, link) => {
 };
 
 exports.sendPasswordResetEmail = async (email, link) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+  await resend.emails.send({
+    from: process.env.EMAIL_FROM,
     to: email,
     subject: "Reset your password",
     html: `
